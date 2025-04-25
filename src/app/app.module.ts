be240ae8be9f65185822de74a8ae27c58d9f11b5
Millimono <millimono64.sm@gmail.com>
2025-04-25 01:47:35 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SitesComponent } from './pages/sites/sites.component';
+import { SiteDetailComponent } from './pages/sites/site-detail/site-detail.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CountryComponent } from './pages/country/country.component';
+import { ReviewsComponent } from './pages/reviews/reviews.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarouselComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    SitesComponent,
+    SiteDetailComponent,
+    AboutComponent,
+    CountryComponent,
+    ReviewsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
